95a39dda253d2da0d9aa5de5124ac043f08b4a6b
GuilleOk <fernandez.martinez1403@gmail.com>
2024-11-09 11:54:02 -0500

    algunos detalles de estilos

--- src/components/CartContent.jsx
+++ src/components/CartContent.jsx
@@ -1,16 +1,24 @@
 /* eslint-disable react/prop-types */
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { CartContext } from '../contexts/Cart'
 import { CartIcon, ClearCartIcon } from './Icons'
 import CartItem from './CartItem'
 
 const CartContent = ({ handleClicIconCart }) => {
   const {cart, cleanCart} = useContext(CartContext)
+  const [counter, setCounter] = useState(0)
+  useEffect(() => {
+    setCounter(0)
+    if (cart !== undefined && cart?.length !== 0) {
+      cart.forEach(item => setCounter(prevState => prevState += item.amount))
+    }
+  }, [cart])
+  
 
   return (
     <div className='cartContent'>
       <div className='cartIconContainer' onClick={handleClicIconCart}>
-        <div className='carticon'><CartIcon /></div>
+        <div className='carticon'><CartIcon /> <span>{counter}</span></div>
       </div>
       {cart?.length === 0
         ? ''
